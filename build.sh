a2e7e22667d9c5c89e63c82ccd75780fef4011ba
Alison C <alisonc@alisonc.net>
2017-10-25 11:36:17 -0400

    comment build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# simple build script to build both public and private forms of resume
+# with and without redacted phone number
+# (c) 2017 Alison Chan alisonc@alisonc.net
+
 pdflatex -jobname=resume '\input{resume}'
 pdflatex -jobname=resume_pub '\def\PUBLIC{}\input{resume}'
 
