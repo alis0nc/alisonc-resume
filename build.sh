b4de724b6b5295deb1bd611f13c50e0b849745ff
Alison C <alisonc@alisonc.net>
2017-11-02 15:45:42 -0400

    tweak build process and add pdfsizeopt

--- build.sh
+++ build.sh
@@ -5,5 +5,20 @@
 
 pdflatex -jobname=resume '\input{resume}'
 pdflatex -jobname=resume_pub '\def\PUBLIC{}\input{resume}'
-pdflatex -jobname=resume_shalem '\def\SHALEM{}\input{resume}'
+pdflatex -jobname=resume_full '\def\SHALEM{}\input{resume}'
+pdflatex -jobname=resume_full_pub '\def\SHALEM{}\def\PUBLIC{}\input{resume}'
 
+# optimise file size
+# TODO: don't assume pdfsizeopt is going to give us any better, actually 
+# check before and after file sizes
+
+type -P "pdfsizeopt" && {
+    pdfsizeopt resume.pdf resume_opt.pdf
+    pdfsizeopt resume_pub.pdf resume_pub_opt.pdf
+    pdfsizeopt resume_full.pdf resume_full_opt.pdf
+    pdfsizeopt resume_full_pub.pdf resume_full_pub_opt.pdf
+    mv resume_opt.pdf resume.pdf
+    mv resume_pub_opt.pdf resume_pub.pdf
+    mv resume_full_opt.pdf resume_full.pdf
+    mv resume_full_pub_opt.pdf resume_full_pub.pdf
+}
