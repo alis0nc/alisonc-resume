9cace5ff6d22352d902e8bd2c8448aedc2954f28
Alison C <alisonc@alisonc.net>
2017-11-28 11:13:08 -0500

    adding per-job-board email address conditionals for better analytics on my end

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ pdflatex -jobname=resume '\input{resume}'
 pdflatex -jobname=resume_pub '\def\PUBLIC{}\input{resume}'
 pdflatex -jobname=resume_full '\def\SHALEM{}\input{resume}'
 pdflatex -jobname=resume_full_pub '\def\SHALEM{}\def\PUBLIC{}\input{resume}'
+pdflatex -jobname=resume_monster '\def\SHALEM{}\def\MONSTER{}\input{resume}'
+pdflatex -jobname=resume_dice '\def\SHALEM{}\def\DICE{}\input{resume}'
 
 # optimise file size
 # TODO: don't assume pdfsizeopt is going to give us any better, actually 
@@ -17,8 +19,12 @@ type -P "pdfsizeopt" && {
     pdfsizeopt resume_pub.pdf resume_pub_opt.pdf
     pdfsizeopt resume_full.pdf resume_full_opt.pdf
     pdfsizeopt resume_full_pub.pdf resume_full_pub_opt.pdf
+    pdfsizeopt resume_monster.pdf resume_monster_opt.pdf
+    pdfsizeopt resume_dice.pdf resume_dice_opt.pdf
     mv resume_opt.pdf resume.pdf
     mv resume_pub_opt.pdf resume_pub.pdf
     mv resume_full_opt.pdf resume_full.pdf
     mv resume_full_pub_opt.pdf resume_full_pub.pdf
+    mv resume_monster_opt.pdf resume_monster.pdf
+    mv resume_dice_opt.pdf resume_dice.pdf
 }
